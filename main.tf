890b2c1f88f8b3a1cf405fdeb0b8ad27e6a97b2f
Peter Pouliot <peter@pouliot.net>
2025-09-25 23:13:43 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -74,7 +74,42 @@ output "default_ssh_private_key" {
 resource "random_uuid" "random_id" { }
 
 # Output: A randomly generated uuid
-output "random_uuid" {
-  value = random_uuid.random_id.result
+output "random_uuid" {# Uses GSuite TXT Record for Verification and adds Googles MX Servers and Zone MX Records
+
+# TXT Record for GMail Verification
+resource "cloudflare_dns_record" "google_site_verification" {
+  count  = "${var.gmail_mx_enable}"
+  domain = "${var.domain_name}"
+  name   = "@"
+  value = "${var.google_site_verification_token}"
+  type   = "TXT"
+  ttl    = 3600
+}
+
+# MX record for Zone
+resource "cloudflare_dns_record" "google_mx" {
+  count = "${length(var.gmail_mx_servers) * var.gmail_mx_enable}"
+  domain = "${var.domain_name}"
+  name = "mail"
+  value = "${var.gmail_mx_servers[count.index]}"
+  priority = "${count.index + 1}"
+  type = "MX"
+  ttl = 3600
+  depends_on = [ "cloudflare_dns_record.google_site_verification" ]
+}
+
+# Add CNAME Records for GSuite Services
+resource "cloudflare_dns_record" "gsuite_aliases" {
+  count = "${length(var.gsuite_aliases) * var.gmail_mx_enable}"
+  domain = "${var.domain_name}"
+  name = "${var.gsuite_aliases[count.index]}"
+  value = "ghs.google.com"
+  type  = "CNAME"
+  ttl = 3600
+  depends_on = [
+    "cloudflare_dns_record.google_site_verification",
+    "cloudflare_dns_record.google_mx",
+  ]
+}  value = random_uuid.random_id.result
   sensitive = false
 }
